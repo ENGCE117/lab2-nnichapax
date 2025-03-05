6adf6ddbc523d0022715d6080c3b5ef877cf8649
nnichapax <nichapha8895@gmail.com>
2025-03-05 22:58:09 +0700

    send 1st

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+
+char* reverse( char str1[]) ;
 
 int main() {
-    //YOUR CODE HERE
-    return 0 ;
+    char text[ 50 ] ;
+    char *output ;
+    output = reverse( text ) ;
+    printf( "\nMAIN : %s", output ) ;
 }//end function
+
+char* reverse( char str1[] ) {
+	char reverse [ 50 ] ;
+
+	printf( "Enter str1 : " ) ;
+    fgets( reverse, sizeof(reverse), stdin ) ;  // ใช้ fgets เพื่อให้รับค่าที่มีช่องว่างได้
+    reverse[strcspn(reverse, "\n")] = '\0' ;
+	// output = reverse(text) ;
+
+	int length = strlen( reverse ) ;
+	int i ;
+
+	for( i = 0 ; i < length ; i++ ) {
+		str1[ i ] = reverse[ length - 1 - i ] ;
+	}
+
+	str1[length] = '\0' ;
+	
+	printf( "FUNC : %s", str1 ) ;
+	// for( i = 0 ; i < length ; i++ ) {
+	// 	printf( "%c", str1[ i ] ) ;
+	// }
+	return str1 ;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+output = reverse(text) ;
